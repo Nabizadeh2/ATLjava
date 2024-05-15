46627f1aa4d5ffd1a55b1e6312aa3b0229c6c307
Ceyhune <nceyhuna@gmail.com>
2024-05-15 17:39:45 +0400

    ObjectFile

---
+++ src/lesson36/PersonFile.java
@@ -0,0 +1,48 @@
+package lesson36;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonFile {
+
+    public static final String RESOURCE = "src/lesson36/resource/";
+
+    public static void main(String[] args) {
+        Person person = new Person("Onur", "Nabizada", 8);
+
+        List<Person>mylist=new ArrayList<>();
+        mylist.add(new Person("Gunel","Hasanova",22));
+        mylist.add(new Person("Xeyransa ","Pashayeva",26));
+        mylist.add(new Person("Shamil","Azaxov",25));
+
+        try (FileOutputStream fos = new FileOutputStream(RESOURCE + "person.ser");
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            oos.writeObject(mylist);
+            oos.writeObject(person);
+        } catch (IOException e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+
+        try (FileInputStream fos = new FileInputStream(RESOURCE + "person.ser");
+             BufferedInputStream bos = new BufferedInputStream(fos);
+             ObjectInputStream oos = new ObjectInputStream(bos)) {
+            List<Person>readList=(List<Person>)oos.readObject();
+            Object object = oos.readObject();
+            System.out.println(object);
+            //if (object instanceof Person) {
+           //     System.out.println(object);
+          //// }
+
+            for(Person person1:readList){
+                System.out.println(person1);
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
